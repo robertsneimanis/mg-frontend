def6c93beca18a8bf47f09ef0978cf695b0acddf
roberts <naimanis.roberts@gmail.com>
2020-10-25 12:56:05 +0000

    styled mobile

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 $(document).ready(function () {
-    $(".show-register").click(function () {
-        $(".form-container").toggleClass("show-register");
-        $(".login-form").fadeOut();
-        $(".register-form").delay(500).fadeIn();
-    });
+    if ($(window).width() > 575) {
+        $(".show-register").click(function () {
+            $(".form-container").toggleClass("show-register");
+            $("#login-form").fadeOut();
+            $("#register-form").delay(500).fadeIn();
+        });
 
-    $(".show-login").click(function () {
-        $(".form-container").toggleClass("show-register");
-        $(".register-form").fadeOut();
-        $(".login-form").delay(500).fadeIn();
-    });
+        $(".show-login").click(function () {
+            $(".form-container").toggleClass("show-register");
+            $("#register-form").fadeOut();
+            $("#login-form").delay(500).fadeIn();
+        });
+    }
 });

dc764f8b0713ab12b02b54d4306547e73d545c3e
roberts <naimanis.roberts@gmail.com>
2020-10-25 07:50:53 +0000

    added register form

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
     $(".login").click(function () {
         $(".form-container").toggleClass("show-register");
+        $(".login-form").toggleClass("d-none");
+        $(".register-form").toggleClass("d-none");
     });
 });
